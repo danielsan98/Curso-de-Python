eb57427de564e20d8a2f6bbfc9c29c280017f09b
danielsan98 <38160558+danielsan98@users.noreply.github.com>
2018-04-07 13:35:04 -0500

    Primeros pasos en programación en python
    Serie de ejercicios y notas sobre la programación en python

---
+++ nombre y edad.py
@@ -0,0 +1,32 @@
+miNombre = "Daniel"
+edad = 19
+
+print("Hola: ", miNombre, "\n", "Tu edad es: ", edad, " años")
+
+#Este es un comentario de 1 linea
+""" este es un comentario que
+puede ser de varias lineas"""
+
+
+#otro programa
+a = b = 15
+c = "Daniel"
+print(c,b)
+
+"""#entrada de datos
+print("Dame tu nombre")
+nombre = input()
+print("Me alegro de conocerle", nombre)
+
+print(nombre,"Vamos a buscar, las esferas del dragon, turururururururururuuruuuuuuuu")"""
+
+#otro programa
+sNombre = str( input("Nombre:"))
+iEdad = input("Edad:")
+iPeso = input("Peso:")
+sFacultad = input("Facultad:")
+fAltura = input("Altura:")
+sEstatus = input("Estatus:")
+sCarrera = input("Carrera:")
+
+print(sNombre, "\n", iEdad, "\n", iPeso, "\n", sFacultad, "\n", fAltura, "\n", sEstatus, "\n", sCarrera)
